26b4fbe859f1765d550c439de6e5e2e8b1c6d600
Vagner Spinola <36640258+VagnerSpinola@users.noreply.github.com>
2019-12-16 11:55:01 +0100

    Update app_compact.py

--- scripts/app_compact.py
+++ scripts/app_compact.py
@@ -1,5 +1,6 @@
 '''
     sera o mesmo App da aplicacao mais compacto onde sera apenas uma funcao para diversos metodos tipo [post, put, delete]...
     
+    Este seria o script a ser entregue ao cliente 
     
 '''
