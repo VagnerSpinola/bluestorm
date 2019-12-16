7e557cb90c10d0d2db2cdc43e074abca7df4b792
Vagner Spinola <36640258+VagnerSpinola@users.noreply.github.com>
2019-12-16 13:55:37 +0100

    Delete app_compact.py

--- scripts/app_compact.py
+++
@@ -1,6 +0,0 @@
-'''
-    sera o mesmo App da aplicacao mais compacto onde sera apenas uma funcao para diversos metodos tipo [post, put, delete]...
-    
-    Este seria o script a ser entregue ao cliente 
-    
-'''
