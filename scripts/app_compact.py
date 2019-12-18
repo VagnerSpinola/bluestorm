c2d075227c0379e11b5804d9f6522785f045288f
Vagner Spinola <36640258+VagnerSpinola@users.noreply.github.com>
2019-12-18 12:56:38 +0100

    Update app_compact.py

--- scripts/app_compact.py
+++ scripts/app_compact.py
@@ -235,7 +235,7 @@ def toker_required(f):
         Verifica se o usuario esta usando um token e se este token e' ainda valido
 
         :type f: string
-        :param f: Nunca parei para pesquisa suas funcao 
+        :param f: E' a funcao a ser docarada pelo decorator 
     '''
 
     @wraps(f)
